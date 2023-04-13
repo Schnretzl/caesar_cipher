b6b6911747289c459804114e79f35547341bb476
Wes Hendley <wrhendley@gmail.com>
2023-04-13 12:34:23 -0500

    Complete function.  Include one successful test case

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,8 +1,23 @@
-def caesar_cipher(input, shift_amount)
-    string.rotate(shift_amount)
+def caesar_cipher(input, shift_amount)    
+    lower = "abcdefghijklmnopqrstuvwxyz".split("")
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+    # input = "Hello!"
+    input = input.split("")
+    # shift_amount = 3
+
+    output_indices = []
+    output = []
+    input.each do |letter|
+        if lower.include?(letter)
+            output << lower.rotate(shift_amount)[lower.find_index(letter)]
+        elsif upper.include?(letter)
+            output << upper.rotate(shift_amount)[upper.find_index(letter)]
+        else
+            output << letter
+        end    
+    end
+    output = output.join
+    puts output
 end
 
-lower = "abcdefghijklmnopqrstuvwxyz".split("")
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-input = "Hello"
-p input.split("")
+caesar_cipher("What a string!", 5)
