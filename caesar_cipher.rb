0897007e90cef1539e8d9637d12e32714200e065
Wes Hendley <wrhendley@gmail.com>
2023-06-27 14:29:51 -0500

    Add more test cases

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -17,4 +17,23 @@ def caesar_cipher(input, shift_amount)
     output = output.join
 end
 
-puts caesar_cipher("What a string!", 5)
+# Test case 1: Encrypting a simple message with a positive shift
+puts caesar_cipher("hello world", 3)  # "khoor zruog"
+
+# Test case 2: Encrypting a message with a negative shift
+puts caesar_cipher("khoor zruog", -3)  # "hello world"
+
+# Test case 3: Encrypting a message with mixed case and punctuation
+puts caesar_cipher("Hello, World!", 5)  # "Mjqqt, Btwqi!"
+
+# Test case 4: Encrypting a message with a shift of 26 (no change)
+puts caesar_cipher("hello world", 26)  # "hello world"
+
+# Test case 5: Encrypting an empty string
+puts caesar_cipher("", 3)  # ""
+
+# Test case 6: Encrypting a message with a large shift
+puts caesar_cipher("hello world", 50)  # "dahhk sknhz"
+
+# Test case 7: Encrypting a message with a shift of 0 (no change)
+puts caesar_cipher("hello world", 0)  # "hello world"
